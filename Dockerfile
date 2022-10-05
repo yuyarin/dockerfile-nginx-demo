f6a354a1875a5bf34be6fa6e48e5d40103c8ac9b
Yuya Kawakami <yuyarin@yuyarin.net>
2022-10-05 10:18:42 +0900

    Display hostname in h1 tag

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM nginxinc/nginx-unprivileged
 USER root
-RUN chown -R nginx /usr/share/nginx
+RUN chown -R 101 /usr/share/nginx
 COPY ./change-hostname.sh /docker-entrypoint.d/change-hostname.sh
+USER 101
+ENTRYPOINT ["/docker-entrypoint.sh"]
+CMD ["nginx", "-g", "daemon off;"]
--- change-hostname.sh
+++ change-hostname.sh
@@ -3,4 +3,5 @@ HOSTNAME=`uname -n`
 if [ -f /usr/share/nginx/html/hostname ]; then
   HOSTNAME=`cat /usr/share/nginx/html/hostname`
 fi
-sed -i "s/<\/title>/ on $HOSTNAME<\/title>/g" /usr/share/nginx/html/index.html
+sed -i "s/!<\/title>/ on $HOSTNAME!<\/title>/g" /usr/share/nginx/html/index.html
+sed -i "s/!<\/h1>/ on $HOSTNAME!<\/h1>/g" /usr/share/nginx/html/index.html
