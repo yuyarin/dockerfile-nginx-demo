c886c4c7b5f4e3f412338def35dc37791bf8f888
Yuya Kawakami <yuyarin@yuyarin.net>
2022-10-06 17:27:16 +0900

    Create env.html

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM nginxinc/nginx-unprivileged
 USER root
 RUN chown -R 101 /usr/share/nginx
+RUN chmod -R o+w /usr/share/nginx
 COPY ./change-hostname.sh /docker-entrypoint.d/change-hostname.sh
 USER 101
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- change-hostname.sh
+++ change-hostname.sh
@@ -5,3 +5,4 @@ if [ -f /usr/share/nginx/html/hostname ]; then
 fi
 sed -i "s/!<\/title>/ on $HOSTNAME!<\/title>/g" /usr/share/nginx/html/index.html
 sed -i "s/!<\/h1>/ on $HOSTNAME!<\/h1>/g" /usr/share/nginx/html/index.html
+env > /usr/share/nginx/html/env.html
